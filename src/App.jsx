fb591141c048f8283176829b08eba5cf45a4a59d
muhammad_ismail <mi272401@gmail.com>
2025-10-01 11:44:23 +0700

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import "./App.css";
+import { useState } from "react";
+import Header from "./components/header";
+import UserInput from "./components/user-input";
+import ResultInvest from "./components/result-invest";
+
+function App() {
+  const [investment, setInvestment] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = investment.duration >= 1;
+
+  function handleInput(inputIdentifier, newValue) {
+    setInvestment((prevInvestment) => {
+      return {
+        ...prevInvestment,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
+  return (
+    <>
+      <Header />
+      <UserInput investment={investment} onChangeInput={handleInput} />
+      {!inputIsValid && <p className="center">please enter valid input data</p>}
+      {inputIsValid && <ResultInvest invest={investment} />}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/result-invest.jsx
@@ -0,0 +1,40 @@
+import { calculateInvestmentResults, formatter } from "../util/investment";
+
+export default function ResultInvest({ invest }) {
+  const resultData = calculateInvestmentResults(invest);
+  const initialInvestment = resultData[0].valueEndOfYear - resultData[0].interest - resultData[0].annualInvestment;
+  console.log("RESULT");
+
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultData.map((investData) => {
+          const totalInterest =
+            investData.valueEndOfYear -
+            investData.annualInvestment * investData.year - initialInvestment;
+
+          const totalAmountInvested = investData.valueEndOfYear - totalInterest
+
+          return (
+            <tr key={investData.year}>
+              <td>{investData.year}</td>
+              <td>{formatter.format(investData.valueEndOfYear)}</td>
+              <td>{formatter.format(investData.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
